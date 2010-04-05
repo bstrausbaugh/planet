c842075fdac879a98aa7650ed26ea1098d60c136
bzs <bzs@bzs>
2010-04-04 22:28:44 -0500

    Progress on time entry from Story view

--- app/controllers/time_entries_controller.rb
+++ app/controllers/time_entries_controller.rb
@@ -1,15 +1,5 @@
 class TimeEntriesController < ApplicationController
-  
-  def new
-    @task = Task.find(params[:task_id])
-    @time_entry = TimeEntry.new
 
-    respond_to do |format|
-      format.html { render :edit }
-      format.xml  { render :xml => @time_entry }
-    end
-  end
-  
   def show
     @time_entry = TimeEntry.find(params[:id])
 
@@ -34,7 +24,6 @@ class TimeEntriesController < ApplicationController
   end
 
   def create
-    
     assign_params_persons
 
     @task = Task.find(params[:task_id])
